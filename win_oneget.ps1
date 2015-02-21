ca279ad7d302b3007d608e749f4d9745046597f8
Trond Hindenes <trond@hindenes.com>
2015-02-21 13:48:36 +0100

    Added a very simple oneget module/updated examples to it to download reqd packages

---
+++ win_oneget.ps1
@@ -0,0 +1,56 @@
+#!powershell
+# (c) 2015, Trond Hindenes <trond@hindenes.com>, and others
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# WANT_JSON
+# POWERSHELL_COMMON
+
+$params = Parse-Args $args;
+$result = New-Object psobject;
+Set-Attr $result "changed" $false;
+
+
+$PackageName = Get-Attr -obj $params -name name -failifempty $true -resultobj $result
+$Force = Get-Attr -obj $params -name force
+$Ensure = Get-Attr -obj $params -name Ensure
+if (!($force)) {$force = $false}
+if (!($Ensure)) {$Ensure = "Present"}
+
+$HasFails = $false
+
+
+$Packages = $Packagename.Split(",")
+foreach ($package in $packages)
+{
+    $package = get-package -name $PackageName -errorAction silentlyContinue
+    if ((!$Package) -and ($ensure -eq "Present"))
+    {
+        $FindPackage = find-package -name $packagename -errorAction SilentlyContinue -ForceBootstrap
+        if (!($FindPackage))
+        {
+            Fail-Json -obj $result -message "Could not find package $package"
+        }
+
+        Install-Package $PackageName -ForceBootstrap -Force
+        Set-Attr $result "changed" $true;
+    
+    }
+
+
+}
+
+Exit-Json -obj $result
---
+++ win_oneget.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2015, Trond Hindenes <trond@hindenes.com>, and others
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+DOCUMENTATION = '''
+---
+module: win_oneget
+version_added: "1.8"
+short_description: Install packages using the OneGet package manager
+description:
+     
+options:
+  refresh_mode:
+    description:
+      - The desired refresh mode. Valid options are "Disabled", "Push", "Pull"
+    required: true
+    default: null
+    aliases: []
+
+author: Trond Hindenes
+'''
+
+EXAMPLES = '''
+# Playbook example
+  - name: Set LCM refresh mode to disabled
+    win_lcm5:
+      refresh_mode="Disabled"
+'''
+
